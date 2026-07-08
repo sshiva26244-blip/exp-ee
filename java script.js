72a4d28164269e66034034e609c96d3c11c98629
sshiva26244-blip <sshiva26244@gmail.com>
2026-07-08 14:52:50 +0530

    Add files via upload

---
+++ java script.js
@@ -0,0 +1,19 @@
+// Function Declaration
+function greet() {
+    console.log("Hello from a function declaration!");
+}
+
+// Function Expression
+const sayGoodbye = function () {
+    console.log("Goodbye from a function expression!");
+};
+
+// Arrow Function
+const sayThanks = () => {
+    console.log("Thanks from an arrow function!");
+};
+
+// Function Calls
+greet();
+sayGoodbye();
+sayThanks();
